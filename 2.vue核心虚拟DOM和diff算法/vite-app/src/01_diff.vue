78bed4c421995bf493e5357f1ce9299a8d9db902
葫芦娃 <78081228+lengdayu@users.noreply.github.com>
2023-03-01 17:14:45 +0800

    ref源码解读

---
+++ 2.vue核心虚拟DOM和diff算法/vite-app/src/01_diff.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="app">
+    <div v-for="(item, index) in arr" :key="index">{{ item }}</div>
+  </div>
+</template>
+<script setup lang="ts">
+const arr: string[] = ["A", "b", "c", "d"];
+arr.splice(2, 0, "DDD");
+</script>
+
+<style scoped></style>
